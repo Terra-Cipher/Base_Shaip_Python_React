9d18868468decff2c92fd57d6c0add5ca0d71c3c
DavoSwain <dave.swain@terracipher.com>
2026-08-01 22:53:41 +1000

    Configures frontend for `/display` base path

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   base: '/display/',
   server: {
     proxy: {
-      '/shaip': {
+      '/display/shaip': {
         target: 'http://127.0.0.1:8080',
         changeOrigin: true,
       },
